1a59b25e5c3c4c64d9274b5c6603f4cf5b97dd78
Pedro Augusto <95659638+PedroAug92@users.noreply.github.com>
2022-12-22 08:13:34 -0300

    Add files via upload

--- 212_softwares.sh
+++ 212_softwares.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+sudo apt update -y && sudo apt upgrade -y
 sudo apt install git gcc neovim wget apache2 mysql-server -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo wget https://github.com/shiftkey/desktop/releases/download/release-2.6.3-linux1/GitHubDesktop-linux-2.6.3-linux1.deb
 sudo dpkg -i GitHubDesktop-linux-2.6.3-linux1.deb
 sudo snap install spotify 
+sudo snap install mysql-workbench-community
 sudo snap install code --classic
 sudo snap install pycharm-community --classic
 sudo snap install eclipse --classic
